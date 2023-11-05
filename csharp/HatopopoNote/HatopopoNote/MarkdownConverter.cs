b079eeb9e5284398ac193af3be6626be4ffd4cbe
Hato <hatomasa1184@gmail.com>
2023-11-05 16:47:48 +0900

    md移動 AddMarkdownConverter

---
+++ csharp/HatopopoNote/HatopopoNote/MarkdownConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace HatopopoNote
+{
+	internal class MarkdownConverter
+	{
+		private static readonly string MarkdownDirectoryPath = "../../md";
+
+		public void Execute()
+		{
+			Console.WriteLine(System.Environment.CurrentDirectory);
+
+			return;
+
+			// ファイル収集
+			var directoryInfo = new DirectoryInfo(MarkdownDirectoryPath);
+
+			foreach (var file in directoryInfo.GetFiles())
+			{
+				Console.WriteLine(file.FullName);
+			}
+		}
+	}
+}
--- csharp/HatopopoNote/HatopopoNote/Program.cs
+++ csharp/HatopopoNote/HatopopoNote/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using HatopopoNote;
+
 Console.WriteLine("Hello, World!");
+
+var markdownConverter = new MarkdownConverter();
+markdownConverter.Execute();
