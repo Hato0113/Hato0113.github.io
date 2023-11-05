abae61726a21a226e00b976f0f23ac15e9da12f3
Hato <hatomasa1184@gmail.com>
2023-11-05 17:29:31 +0900

    mdファイルを取得する処理を追加

--- csharp/HatopopoNote/HatopopoNote/MarkdownConverter.cs
+++ csharp/HatopopoNote/HatopopoNote/MarkdownConverter.cs
@@ -1,22 +1,42 @@
-﻿namespace HatopopoNote
+﻿using System.Net.NetworkInformation;
+
+namespace HatopopoNote
 {
 	internal class MarkdownConverter
 	{
-		private static readonly string MarkdownDirectoryPath = "../../md";
+		private static readonly string MarkdownDirectoryName = "md";
 
 		public void Execute()
 		{
-			Console.WriteLine(System.Environment.CurrentDirectory);
+			if (!TryGetTargetDirectory(out var targetDirectory)) return;
+			Console.WriteLine($"target : {targetDirectory!.FullName}");
 
-			return;
-
-			// ファイル収集
-			var directoryInfo = new DirectoryInfo(MarkdownDirectoryPath);
-
-			foreach (var file in directoryInfo.GetFiles())
+			// .mdのファイルを取得
+			var files = new List<FileInfo>();
+			foreach ( var file in targetDirectory.GetFiles("*.md",SearchOption.AllDirectories))
 			{
-				Console.WriteLine(file.FullName);
+				Console.WriteLine($"{file.FullName}");
 			}
+		}
+
+		private bool TryGetTargetDirectory(out DirectoryInfo? targetDirectory)
+		{
+			// mdが見つかるまでさかのぼる
+			var currentDirectory = new DirectoryInfo(System.Environment.CurrentDirectory);
+			targetDirectory = null;
+			while (targetDirectory == null && currentDirectory != null)
+			{
+				var directory = currentDirectory.GetDirectories(MarkdownDirectoryName);
+				if (directory.Length != 0)
+				{
+					targetDirectory = directory[0];
+					return true;
+				}
+
+				currentDirectory = currentDirectory.Parent;
+			}
+
+			return false;
 		}
 	}
 }
